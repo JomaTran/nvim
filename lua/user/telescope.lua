b4c887df4cf5e59346431b8ed9207fbe43de0ea5
Hai Tran <hai.tran1@tyme.com>
2022-06-23 23:56:26 +0700

    first commit

---
+++ lua/user/telescope.lua
@@ -0,0 +1,34 @@
+local actions = require "telescope.actions"
+
+require("telescope").setup {
+  defaults = {
+
+    selection_caret = " ",
+    path_display = { "smart" },
+
+    mappings = {
+      i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<esc>"] = actions.close
+      },
+
+      n = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<esc>"] = actions.close
+      },
+    },
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+  }
+}
